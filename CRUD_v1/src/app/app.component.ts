3706c3dbe2d8f766727186e0e0a6c66341ad113b
Albert <albert.sibiga.dev@gmail.com>
2022-09-06 19:14:32 +0200

    Added basic dialog

--- CRUD_v1/src/app/app.component.ts
+++ CRUD_v1/src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { DialogComponent } from './dialog/dialog.component';
+import { MatDialog } from "@angular/material/dialog";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'CRUD_v1';
+
+  constructor(public dialog: MatDialog) {
+    }
+
+
+    openDialog(height: number = 200, width: number = 200) {
+      this.dialog.open(DialogComponent,{
+        width: `${width}px`,
+        height: `${height}px`,
+      });
+    }
+
+
+
+
 }
--- CRUD_v1/src/app/app.module.ts
+++ CRUD_v1/src/app/app.module.ts
@@ -4,13 +4,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { DialogComponent } from './dialog/dialog.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DialogComponent,
   ],
+  entryComponents: [],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
